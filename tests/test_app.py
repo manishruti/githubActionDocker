a79eaa3b9a0c989163e07aa9378b7bf9609ec9af
manishruti <ojhamnsh@gmail.com>
2025-09-14 17:30:03 +0530

    9th commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,4 @@ from src.app import app
 def test_home_route():
     response = app.test_client().get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello world"
+    assert response.data == b"Hello world to the new world"
